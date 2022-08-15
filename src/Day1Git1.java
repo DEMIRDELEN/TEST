2fdb2f1ac83df3ec021af4772ff76dae2d62aa6f
Mehmet Furkan <demirdelen12012@gmail.com>
2022-08-15 20:22:00 +0300

    Furkan3

--- src/Day1Git1.java
+++ src/Day1Git1.java
@@ -1,5 +1,8 @@
 public class Day1Git1 {
     public static void main(String[] args) {
+
+        System.out.println("FURKAN");
+        System.out.println("FURKAN");
         System.out.println("FURKAN");
     }
 }
